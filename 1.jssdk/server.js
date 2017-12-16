dbd7924a8ea611a1f503a42bd44ef6ff63573670
zhufengnodejs <zhufengnodejs@126.com>
2017-12-16 20:16:24 +0800

    qqlogin

--- 1.jssdk/server.js
+++ 1.jssdk/server.js
@@ -2,7 +2,7 @@ let express = require('express');
 let path = require('path');
 let app = express();
 app.use(express.static(__dirname));
-app.get('/login/bind/qq/callback',function(req,res){
+app.get('/oauth/callback/qqconn',function(req,res){
   res.sendFile(path.resolve('./callback.html'));
 });
 app.listen(80);
